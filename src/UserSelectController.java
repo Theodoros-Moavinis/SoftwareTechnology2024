2d74c1e432090a78f076ac6b839b12242e4e48fd
Theodoros-Moavinis <ics23054@uom.edu.gr>
2024-05-29 20:07:33 +0300

    κλείνει το παράθυρο κανονικά

--- src/UserSelectController.java
+++ src/UserSelectController.java
@@ -5,9 +5,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class UserSelectController {
-
+    //έχουν W μπροστά γιατί είναι στο Welcome view.
     @FXML
     private Button GuestBtn;
 
@@ -20,11 +21,17 @@ public class UserSelectController {
     @FXML
     void GuestBtn_Clicked(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("guest_view.fxml"));
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
+            //FXMLLoader loader = new FXMLLoader(getClass().getResource("guest_view.fxml"));
+            //Parent root = loader.load();
+            //Stage stage = new Stage();
+            //stage.setScene(new Scene(root));
+            //stage.show();
+
+            Parent root  = FXMLLoader.load(getClass().getResource("guest_view.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            Scene scene =  new Scene(root);
+            stage.setScene(scene);
 
             //κάπως πρέπει το προηγούμενο stage να κλείνει.
         } catch (Exception e) {
