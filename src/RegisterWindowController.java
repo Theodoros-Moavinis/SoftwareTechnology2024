6d9c2de44b90087bb769fa1fc00303fad78d87cb
symeonalmanidis <ics23083@uom.edu.gr>
2024-06-23 18:27:36 +0300

    Ολοκλήρωση τεστ για την εγγραφή

--- src/RegisterWindowController.java
+++ src/RegisterWindowController.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 
+import com.sun.swing.internal.plaf.basic.resources.basic_pt_BR;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +10,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Control;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -15,7 +19,7 @@ import javafx.stage.Stage;
 
 public class RegisterWindowController extends Controller {
 
-    
+    private boolean fieldsAreValid = false;
     @FXML
     private Button BackButton;
 
@@ -29,6 +33,8 @@ public class RegisterWindowController extends Controller {
     private TextField LastNameField;
 
     @FXML
+    private DatePicker BirthDayField;
+    @FXML
     private TextField EmailField;
 
     @FXML
@@ -40,6 +46,9 @@ public class RegisterWindowController extends Controller {
     @FXML
     private TextField CityField;
     
+    @FXML
+    private TextField GenderChoice;
+
     @FXML
     private TextField UsernameField;
 
@@ -79,55 +88,97 @@ public class RegisterWindowController extends Controller {
 
     }
 
-    
-    boolean inputIsValid() {
-        boolean isValid = true;
-        ErrorTextArea.setText("");
-        if (FirstNameField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("First name required\n");
-            isValid = false;
-        }
+    void colorFieldIf(Control field, boolean hasCorrectInput)
+    {
+        if (hasCorrectInput)
+            field.setStyle("-fx-control-inner-background: white;");
+        else
+            field.setStyle("-fx-control-inner-background: red;");
 
-        if (LastNameField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("Last name required\n");
-            isValid = false;
-        }
+    }
+    void testIfFieldEmpty(TextField field, String message) {
+        boolean fieldIsValid = field.getText().trim().isEmpty();
+        if (fieldIsValid) {
+            ErrorTextArea.appendText(message + '\n');
+            fieldsAreValid = false;
+        } 
+        colorFieldIf(field, !fieldIsValid);
+    }
 
-        if (EmailField.getText().trim().isEmpty()) {
+    void testBirthday() {
+        boolean birthdayIsValid = true;
+        if(BirthDayField.getValue() == null) {
+            ErrorTextArea.appendText("Birth Day can't be empty\n");
+            birthdayIsValid = false;
+        }
+        colorFieldIf(BirthDayField, birthdayIsValid);
+    }
+
+    void testEmail() {
+        boolean emailIsValid = true;
+        String email = EmailField.getText().trim();
+        if (email.isEmpty()) {
             ErrorTextArea.appendText("Email required\n");
-            isValid = false;
+            emailIsValid = false;
+            fieldsAreValid = false;
         }
+        else if (!email.contains("@")) {
+            ErrorTextArea.appendText("Email incorrect\n");
+            emailIsValid = false;
+            fieldsAreValid = false;
+        } 
+        colorFieldIf(EmailField, emailIsValid);
+        
+    }
 
-        if (PhoneNumberField.getText().trim().length() != 10) {
-            ErrorTextArea.appendText("Phone number must be 10 digits\n");
-            isValid = false;
-        }
+    void testPhone() {
+        boolean phoneIsValid = false;
+        if (PhoneNumberField.getText().trim().length() != 10 
+        || !PhoneNumberField.getText().trim().startsWith("69")) {
+            ErrorTextArea.appendText("Phone incorrect\n");
+            phoneIsValid = false;
+            fieldsAreValid = false;
+        } 
+        colorFieldIf(PhoneNumberField, phoneIsValid);
+    }
 
-        if (EmailField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("Email required\n");
-            isValid = false;
-        }
-
-        if (UsernameField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("Username cannot be empty\n");
-            isValid = false;
-        }
-
-        if (PasswordField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("Password cannot be empty\n");
-            isValid = false;
-        }
-
-        if (RepeatPasswordField.getText().trim().isEmpty()) {
-            ErrorTextArea.appendText("Repeat password cannot be empty\n");
-            isValid = false;
-        }
-
-        if (PasswordField.getText().equals(RepeatPasswordField.getText())) {
+    void testPassword() {
+        boolean passwordIsValid = true;
+        String password = PasswordField.getText().trim();
+        String repeatPassword = RepeatPasswordField.getText().trim();
+        if (password.length() < 8 
+        || !password.matches("[A-Z]") 
+        || !password.matches("[0-9]")
+        || !password.matches("[#?!]")) {
+            ErrorTextArea.appendText("Password incorrect. Must have atleast 8 letters, 1 digit and 1 special character (#?!)\n");
+            passwordIsValid = false;
+            fieldsAreValid = false;
+        } else if (!password.equals(repeatPassword)) {
             ErrorTextArea.appendText("Repeat password must be the same\n");
-            isValid = false;
+            passwordIsValid = false;
+            fieldsAreValid = false;
         }
-        return isValid;
+        colorFieldIf(PasswordField, passwordIsValid);
+        colorFieldIf(RepeatPasswordField, passwordIsValid);
+    }
+
+
+    boolean inputIsValid() {
+        fieldsAreValid = true;
+        ErrorTextArea.setText("");
+
+        testIfFieldEmpty(FirstNameField, "First name required");        
+        testIfFieldEmpty(LastNameField, "Last name required");
+        testBirthday();     
+        testEmail();
+        testPhone();
+        testIfFieldEmpty(CountryChoice, "Country required");
+        testIfFieldEmpty(CityField, "City required");
+        testIfFieldEmpty(GenderChoice, "Gender required");
+        testIfFieldEmpty(UsernameField, "Username required");
+        testPassword();
+
+        return fieldsAreValid;
     }
 
 }
